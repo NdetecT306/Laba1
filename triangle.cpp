5de35e9e772442bbb1fa30c581412c98f9519892
Ndetect306 <nstasya777@mail.ru>
2025-03-14 09:27:43 +0700

    update triangle.cpp

--- triangle.cpp
+++ triangle.cpp
@@ -5,6 +5,29 @@ using namespace std;
 
 void calculateTriangle(double a, double b, double c) 
 {
+    while(a<0 || b<0 || c<0)
+    {
+	cout << "Неверные значения. Попробуйте снова."<<endl;
+	cout << "Введите значения:";
+	cin >> a >> b >> c;
+    }
+    double max = 0, sum = 0;
+    if(a<=b && c<=b)
+    {
+	max = b;
+	sum = a+c;
+    }
+    else if(a<=c && b<=c)
+    {
+        max = c;
+        sum = a+b;
+    }
+    else  if(b<=a && c<=a)
+    {
+        max = a
+        sum = b+c;
+    }
+
     double perimeter = a + b + c;// Периметр
     double s = perimeter / 2;// Полупериметр для формулы Герона
     double area = sqrt(s * (s - a) * (s - b) * (s - c));    // Площадь по формуле Герона

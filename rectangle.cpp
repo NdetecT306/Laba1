4efab289e0b79677eb6325aaf55835fadee773e9
sashafp6 <filippovaalexa@mail.ru>
2025-03-14 08:57:16 +0700

    Update rectangle.cpp

--- rectangle.cpp
+++ rectangle.cpp
@@ -6,15 +6,15 @@
 using namespace std;
 
 void calculateRectangle(double width, double height) {
+    if width > 0 and height > 0:
     // Периметр
-    double perimeter = 2 * (width + height);
+        double perimeter = 2 * (width + height);
 
     // Площадь
-    double area = width * height;
+        double area = width * height;
 
     // Длина диагонали
-    double diagonal = sqrt(width * width + height * height);
-if width > 0 and height > 0:
+        double diagonal = sqrt(width * width + height * height);
     // Вывод результатов
     cout << "Периметр прямоугольника: " << perimeter << "\n";
     cout << "Площадь прямоугольника: " << area << "\n";
